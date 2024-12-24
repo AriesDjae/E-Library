ffbdf6d823dbe2b198456fb24e3822ea1646e1cc
Muhammad Aries Djaenuri <ariesdjaenuri24@gmail.com>
2024-12-24 09:20:38 +0700

    Update Signup.php

--- Register-Login/Signup.php
+++ Register-Login/Signup.php
@@ -27,9 +27,12 @@ unset($_SESSION['old_input']);
                     <span>A</span>
                 </label>
                 <input type="text" name="namalengkap" placeholder="Name">
+            </div>
+            <div>
                 <label for="username">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-480q-66 0-113-47t-47-113q0-66 47-113t113-47q66 0 113 47t47 113q0 66-47 113t-113 47ZM160-160v-112q0-34 17.5-62.5T224-378q62-31 126-46.5T480-440q66 0 130 15.5T736-378q29 15 46.5 43.5T800-272v112H160Z"/></svg>
                 </label>
+                
                 <input type="text" name="username" placeholder="Username" value="<?php echo htmlspecialchars($old_input['username'] ?? ''); ?>" required>
             </div>
             <div>
@@ -48,19 +51,19 @@ unset($_SESSION['old_input']);
                 <label for="repeat-password-input">
                     <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M240-80q-33 0-56.5-23.5T160-160v-400q0-33 23.5-56.5T240-640h40v-80q0-83 58.5-141.5T480-920q83 0 141.5 58.5T680-720v80h40q33 0 56.5 23.5T800-560v400q0 33-23.5 56.5T720-80H240ZM240-200q33 0 56.5-23.5T320-280q0-33-23.5-56.5T240-360q-33 0-56.5 23.5T160-280q0 33 23.5 56.5T240-200ZM160-640h240v-80q0-50-35-85t-85-35q-50 0-85 35t-35 85v80Z"/></svg>
                 </label>
-                <input type="password" name="repeat-password" id="repeat-password-input" placeholder="Konfirmasi Password">
+                <input type="password" name="repeat-password" id="repeat-password-input" placeholder="Confirm Password">
             </div>
             <div>
                 <label for="telepon">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-telephone-fill" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M1.885.511a1.745 1.745 0 0 1 2.61.163L6.29 2.98c.329.423.445.974.315 1.494l-.547 2.19a.68.68 0 0 0 .178.643l2.457 2.457a.68.68 0 0 0 .644.178l2.189-.547a1.75 1.75 0 0 1 1.494.315l2.306 1.794c.829.645.905 1.87.163 2.611l-1.034 1.034c-.74.74-1.846 1.065-2.877.702a18.6 18.6 0 0 1-7.01-4.42 18.6 18.6 0 0 1-4.42-7.009c-.362-1.03-.037-2.137.703-2.877z"/></svg>
                 </label>
-                <input type="number" name="telepon" id="telepon" placeholder="Nomor Telepon">
+                <input type="number" name="telepon" id="telepon" placeholder="Phone Number">
             </div>
             <div>
                 <label for="alamat">
                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-geo-alt-fill" viewBox="0 0 16 16"><path d="M8 16s6-5.686 6-10A6 6 0 0 0 2 6c0 4.314 6 10 6 10m0-7a3 3 0 1 1 0-6 3 3 0 0 1 0 6"/></svg>
                 </label>
-                <input type="text" name="alamat" id="alamat" placeholder="Alamat">
+                <input type="text" name="alamat" id="alamat" placeholder="Address">
             </div>
             <div>
                 <label for="tipe">
